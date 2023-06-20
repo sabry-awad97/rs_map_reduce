ba96bde3ab2374a3bb36429dc7bb7de7672b1bdb
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-20 16:51:35 +0300

    🎉 feat(main.rs): Add line counting functionality 📝 chore(.gitignore): Add test input directory to ignore list The `main.rs` file has been updated to include a line counting functionality. A `Worker` trait has been defined with `map`, `reduce`, and `get_result` methods. A `LineCountWorker` struct has been defined that implements the `Worker` trait and counts the number of lines in the input data. A `FileInputData` struct has also been defined that implements the `GenericInputData` trait and reads input data from a file. The `main` function has been updated to use these new structs and print the number of lines in the input data.
    Additionally, the `.gitignore` file has been updated to include a `test_inputs` directory to ignore.
    
    Changes made:
    - Added `GenericInputData` trait with `read` method to read input data.
    - Added `Worker` trait with `map`, `reduce`, and `get_result` methods.
    - Added `LineCountWorker` struct that implements the `Worker` trait and counts the number of lines in the input data.
    - Added `FileInputData` struct that implements the `GenericInputData` trait and reads input data from a file.
    - Updated `main` function to use `LineCountWorker` and `FileInputData` structs and print the number of lines in the input data.
    - Updated `.gitignore` file to include `test_inputs` directory.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::{fs::File, io::Read, path::PathBuf};
+
+trait GenericInputData {
+    fn read(&self) -> String;
+}
+
+trait Worker {
+    fn map(&mut self);
+    fn reduce(&mut self, other: &dyn Worker);
+    fn get_result(&self) -> usize;
+}
+
+struct LineCountWorker {
+    input_data: Box<dyn GenericInputData>,
+    result: usize,
+}
+
+impl Worker for LineCountWorker {
+    fn map(&mut self) {
+        let data = self.input_data.read();
+        self.result = data.lines().count();
+    }
+
+    fn reduce(&mut self, other: &dyn Worker) {
+        self.result += other.get_result();
+    }
+
+    fn get_result(&self) -> usize {
+        self.result
+    }
+}
+
+struct FileInputData {
+    file_path: PathBuf,
+}
+
+impl GenericInputData for FileInputData {
+    fn read(&self) -> String {
+        let mut file = File::open(&self.file_path).expect("Failed to open file");
+        let mut content = String::new();
+        file.read_to_string(&mut content)
+            .expect("Failed to read file");
+        content
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_data = Box::new(FileInputData {
+        file_path: PathBuf::from("test_inputs/1"),
+    });
+
+    let mut worker = LineCountWorker {
+        input_data,
+        result: 0,
+    };
+
+    worker.map();
+
+    println!("Lines: {}", worker.get_result());
 }
